67031f29de6234e929a9fb3d6fc0abaea51a008f
Phuc Do <ngocphucdo2601@gmail.com>
2023-05-13 15:09:41 +0700

    06-Scalar type basic b01

---
+++ les06ScalarType.pl
@@ -0,0 +1,17 @@
+=blockComment
+    - Scalar is variable can hold an integer, astring, a float, or ...
+    - No specific type mentioned in like integer, string, or floating
+    - Numeric Scalar, String scalar, float scalar
+    - Declare a Scalar
+        Symbol - $
+        $name = "Bill";
+
+=cut
+
+$numScalar = 100;
+
+$strScalar = "Perl";
+
+$vString = v85.78.73.88;
+
+print $numScalar."-".$strScalar."-".$vString."\n";
