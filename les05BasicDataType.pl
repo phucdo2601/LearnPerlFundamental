c198042778bca027efa93ef28c9accfe25e2b9e2
Phuc Do <ngocphucdo2601@gmail.com>
2023-05-13 15:02:08 +0700

    05-Basic Data type b01

---
+++ les05BasicDataType.pl
@@ -0,0 +1,10 @@
+# Scalar
+$string = "Learn Perl";
+
+# Arrays
+@listFruit = ("Orange", "Apple", "Grapes");
+
+# Hashes
+%animalHash = (1=> "Dog", 2=> "Cat");
+
+print "$string\n@listFruit\n",%animalHash,"";
