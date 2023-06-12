096bb3df2dc1e0236684d5632e3442869eaa31f8
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-06-12 20:45:57 +0700

    First learn basic template and pro structure in Mojolicious b01

---
+++ my_app/lib/MyApp/Controller/Example.pm
@@ -0,0 +1,11 @@
+package MyApp::Controller::Example;
+use Mojo::Base 'Mojolicious::Controller', -signatures;
+
+# This action will render a template
+sub welcome ($self) {
+
+  # Render template "example/welcome.html.ep" with message
+  $self->render(msg => 'Welcome Phucdn!');
+}
+
+1;
